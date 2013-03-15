fb4d43ff3601d3d22bf22d84fb42fe3d4a8a3bf2
Dion Moses <phoncey@gmail.com>
2013-03-15 13:00:52 -0700

    added better message to print statement

--- TestClean.pl
+++ TestClean.pl
@@ -5,10 +5,11 @@ $content = "use 'git clean -d -x -f' to remove untracked files, including direct
 for($count = 0; $count < 10; $count++){
 
     `mkdir dir_$count`;
-    `echo $content > file_$count.txt`;
+    `echo "$content" > file_$count.txt`;
     `cp file_$count.txt dir_$count`;
 }
 
 #if next line is uncommented a git checkout is required to bring it back to normal
 #`echo "$content" >> WorkMarkReadMe.txt`;
+print "I made this primarily as a way to test out the ability to clean a working directory out once you've dirties it up with a bunch of files, from a build for example...\n";
 print $content;
