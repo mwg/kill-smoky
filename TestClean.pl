b29b20a0030d4db138a01be656949f71bb9ffc04
Dion Moses <phoncey@gmail.com>
2013-03-29 12:59:45 -0700

    Change to TestClean.pl

--- TestClean.pl
+++ TestClean.pl
@@ -10,6 +10,6 @@ for($count = 0; $count < 10; $count++){
 }
 
 #if next line is uncommented a git checkout is required to bring it back to normal
-#`echo "$content" >> WorkMarkReadMe.txt`;
+`echo "$content" >> WorkMarkReadMe.txt`;
 print "I made this primarily as a way to test out the ability to clean a working directory out once you've dirties it up with a bunch of files, from a build for example...\n";
 print $content;
