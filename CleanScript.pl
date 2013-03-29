2757c5d8e8dc4b170bec07ae2295d5ac1c6d5075
Dion Moses <phoncey@gmail.com>
2013-03-29 13:09:34 -0700

    Added CleanScript.pl

---
+++ CleanScript.pl
@@ -0,0 +1,16 @@
+#!/opt/local/bin/perl
+
+use strict;
+
+print "Removing untracked files...\n";
+`git clean -d -x -f`;
+
+print "Reverting modified files...\n"
+my @gitstatus = `git status`;
+
+foreach my $line (@gitstatus){
+    if($line =~ /modified:\s+(.+)/){
+        print "Reverting $1...\n";
+        `git checkout $1`;
+    }
+}
